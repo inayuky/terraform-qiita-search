150ec864715e38da1648d20898b32da1670f39d1
inayuky <inayuky@gmail.com>
2021-05-30 04:19:24 +0900

    update docker compose version

--- modules/instance/install_fess.sh
+++ modules/instance/install_fess.sh
@@ -6,7 +6,7 @@ systemctl enable docker
 systemctl start docker
 
 # install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 

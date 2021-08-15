f2c1ef10c09e963dcc3ed8dfcf35d0f00be4a7a2
inayuky <inayuky@gmail.com>
2021-08-15 17:25:39 +0900

    Revert "remove docker-compose"
    This reverts commit e9034d9dafb8e689e4360a3c58f90d079cf67583.

--- modules/instance/install_fess.sh
+++ modules/instance/install_fess.sh
@@ -8,6 +8,11 @@ amazon-linux-extras install -y docker
 systemctl enable docker
 systemctl start docker
 
+# install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 # change parameter for Elasticsearch
 echo "vm.max_map_count=262144" >> /etc/sysctl.conf
 sysctl -q -w vm.max_map_count=262144
@@ -16,4 +21,4 @@ sysctl -q -w vm.max_map_count=262144
 yum install -y git
 git clone https://github.com/inayuky/docker-fess.git
 cd docker-fess/compose
-docker compose -f docker-compose.yml -f docker-compose.standalone.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.standalone.yml up -d
